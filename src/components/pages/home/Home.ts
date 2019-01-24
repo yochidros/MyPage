c06bd2da76b6ddd3173207e51c9710df153b18b1
yochidrop <miyazawa@nagisa-inc.jp>
2019-01-24 18:53:12 +0900

    rename: project name MyHomePage.

--- src/components/pages/home/Home.ts
+++ src/components/pages/home/Home.ts
@@ -3,6 +3,7 @@ import CardCollection from '@/components/molecules/CardCollection/CardCollection
 import { Getters as GithubGetters } from '@/store/modules/github/getters';
 import {Getter} from 'vuex-class';
 import Spinner from '@/components/atoms/Spinner/Spinner';
+// @ts-ignore
 import Timeline from 'vue-tweet-embed/src/timeline';
 
 @Component({
